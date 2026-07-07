3cf9e07735085c4b89f7effc2411d407f52cf632
shabdv3476 <shabdv.bu@gmail.com>
2026-07-07 19:32:25 +0530

    Add files via upload

---
+++ sum of Array.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int sumOfArray(int arr[], int size) {
+    int sum = 0;
+
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << sumOfArray(arr, n);
+
+    return 0;
+}
